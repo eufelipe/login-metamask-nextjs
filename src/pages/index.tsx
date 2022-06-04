b64cdeb57125b14bf106032ba84423ef62788663
Felipe Rosas <contato@eufelipe.com>
2022-06-04 13:05:18 -0300

    chore: update layout main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box, Heading, Text } from "@chakra-ui/react";
 import { ConnectButton } from "@components/ConnectButton";
 
 export default function Main() {
   return (
-    <Flex justify="center" alignItems="center" bg="white" p="10">
-      <ConnectButton />
+    <Flex h="100vh" alignItems="stretch">
+      <Flex
+        maxWidth="600"
+        width="100%"
+        direction="column"
+        placeContent="center"
+        p="10"
+      >
+        <Heading as="h1" size="4xl" noOfLines={1}>
+          Hi there 👋
+        </Heading>
+        <Text as="h2" fontSize="2xl">
+          Welcome to web 3.0
+        </Text>
+        <ConnectButton />
+      </Flex>
+      <Box
+        flex="1"
+        bgSize="cover"
+        bgImage={"url(/images/main-background.png)"}
+      />
     </Flex>
   );
 }
