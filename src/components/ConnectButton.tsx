8ce2f31d9d8dfa36fb438001b5fab92584ae0f6f
Felipe Rosas <contato@eufelipe.com>
2022-06-04 00:18:33 -0300

    chore: add connect with metamask

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,7 +1,20 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 import { Button } from "@chakra-ui/react";
 
 function ConnectButton() {
-  return <Button>Connect to your wallet</Button>;
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+
+  return (
+    <>
+      {address ? (
+        <h4>Connected as {address}</h4>
+      ) : (
+        <Button onClick={connectWithMetamask}>Connect to your wallet</Button>
+      )}
+    </>
+  );
 }
 
 export { ConnectButton };
